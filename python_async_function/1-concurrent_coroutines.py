e5e7536dd8b689f22d20a2f684e8eb346607e00e
Flagelo6482 <frankln6482@gmail.com>
2023-09-02 09:56:47 -0700

    Feat: Adding documentation :rocket:

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Asynchronous routine that takes 2 integer arguments"""
     tasks = []
 
     for _ in range(n):
