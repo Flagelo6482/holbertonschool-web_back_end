b04e9627adbc3a783982952ff2c92874a4bc116e
Flagelo6482 <frankln6482@gmail.com>
2023-09-03 19:57:00 -0700

    Feat: Update :rocket:

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,12 +9,10 @@ async def measure_runtime() -> float:
     """coroutine that will execute async_comprehension four
        times in parallel using asyncio.gather."""
     start_time = asyncio.get_event_loop().time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
     end_time = asyncio.get_event_loop().time()
     total_runtime = end_time - start_time
     return total_runtime
