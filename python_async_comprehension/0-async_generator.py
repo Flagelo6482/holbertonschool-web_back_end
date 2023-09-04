f88184642c2586fd85724f1060eaeb075fe6c75b
Flagelo6482 <frankln6482@gmail.com>
2023-09-03 19:29:51 -0700

    Feat: Update annotation :rocket:

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ Async Generator"""
 import random
 import asyncio
+from typing import Generator, Optional
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """coroutine called async_generator that takes no arguments"""
     for _ in range(10):
         await asyncio.sleep(1)
