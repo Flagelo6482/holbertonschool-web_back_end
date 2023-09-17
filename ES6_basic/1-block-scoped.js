db6350d1d0389f8ec8f04e14c01a6823fb2706ab
Flagelo6482 <frankln6482@gmail.com>
2023-09-17 16:26:54 +0000

    Feat: Update :rocket:

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  return [task, task2];
+  const task_1 = false;
+  const task2_1 = true;
 
   if (trueOrFalse) {
     var task1 = false;
     var task22 = true;
   }
+
+  return [task_1, task2_1];
 }
