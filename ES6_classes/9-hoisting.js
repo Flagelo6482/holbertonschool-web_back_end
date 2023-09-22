e9cd0d0a77bbc415931843decac537c7c3233885
Flagelo6482 <frankln6482@gmail.com>
2023-09-22 23:58:45 +0000

    Feat: Update class :rocket :

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,17 +1,17 @@
 export class HolbertonClass {
-    constructor(year, location) {
-      this._year = year;
-      this._location = location;
-    }
-  
-    get year() {
-      return this._year;
-    }
-  
-    get location() {
-      return this._location;
-    }
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
   }
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
+}
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
