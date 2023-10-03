051949f5fb9bc6b7822f26dea41e96bf648fae0c
Flagelo6482 <frankln6482@gmail.com>
2023-10-03 15:34:19 +0000

    Feat: All promises :rocket:

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+  .then((values) => {
+    const bodyPhoto = values[0].body;
+    const firstName = values[1].firstName;
+    const lastName = values[1].lastName;
+
+    console.log(`${bodyPhoto} ${firstName} ${lastName}`);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+}
