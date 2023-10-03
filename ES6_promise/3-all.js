07469eb1f56046636cbdabfe081326f4d50e1fc1
Flagelo6482 <frankln6482@gmail.com>
2023-10-03 15:39:17 +0000

    Feat: Update identation :rocket:

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,14 +2,13 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-  .then((values) => {
-    const bodyPhoto = values[0].body;
-    const firstName = values[1].firstName;
-    const lastName = values[1].lastName;
-
-    console.log(`${bodyPhoto} ${firstName} ${lastName}`);
-  })
-  .catch((error) => {
-    console.log('Signup system offline');
-  });
-}
+    .then((values) => {
+      const bodyPhoto = values[0].body;
+      const firstName = values[1].firstName;
+      const lastName = values[1].lastName;
+      console.log(`${bodyPhoto} ${firstName} ${lastName}`);
+    })
+    .catch((error) => {
+      console.log('Signup system offline');
+    });
+  }
