0a28ad1f48b3722fad8b85c166b1573b5d29e652
Flagelo6482 <frankln6482@gmail.com>
2023-10-03 14:44:26 +0000

    Feat: Update function :rocket:

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,13 @@
 export default function handleResponseFromAPI(promise) {
-    return new Promise((resolve, reject) => {
-        if(resolve) {
-            console.log('Got a response from the API');
-            resolve({ status: 200, body: 'success' });
-        } else {
-            console.log('Got a response from the API');
-            reject(Error);
-        }
+  return new Promise((resolve, reject) => {
+    promise
+    .then((response) => {
+      console.log('Got a response from the API');
+      resolve({ status: 200, body: 'success' });
     })
+    .catch((error) => {
+      console.log('Got a response from the API');
+      reject(new Error('Error al rechazar'));
+    });
+  })
 }
